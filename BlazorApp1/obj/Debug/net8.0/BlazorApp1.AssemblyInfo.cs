1f08dc35e84fe82b6499815fa4c8ac561c7b6f2e
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-11-23 20:01:32 +0100

    chore: update .gitignore patterns and AssemblyInformationalVersion

--- BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a005c56980a0f70281440abe643d1eee7fc3a1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ac6b19eec0bbb2a252cb9118afe760fb8830398")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
